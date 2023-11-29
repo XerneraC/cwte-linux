384088e8edfbde5e694191f59b9db441415ccea3
Marc Meier <xernerac@gmail.com>
2023-11-29 01:10:07 +0000

    Update Manifest

--- remanif.sh
+++ remanif.sh
@@ -2,11 +2,8 @@
 
 rm -R metadata/md5-cache
 rm */*/Manifest
-pkgdev manifest
-ebuilds=$(ls */*/*.ebuild)
-for curr_ebuild in $ebuilds; do
-	ebuild $curr_ebuild manifest
-done
-ls */*/*
+pkgdev manifest -f
+ls */*/Manifest
+ls metadata/md5-cache/*
 pkgcheck scan
 
